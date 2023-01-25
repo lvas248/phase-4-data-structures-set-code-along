02998886e1668810f32fc92b6f16918ef5001401
Luis <lvas248@gmail.com>
2023-01-25 17:14:33 -0500

    commit tree 0003ed2cc54449393fdcfed04fbc3440e55350bf

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,33 @@
 # your code here
+
+class MySet
+
+    attr_accessor :array
+
+    def initialize(enumerable =[])
+        @hash = {}
+        enumerable.each do |value|
+            @hash[value] = true
+        end
+
+    end
+
+    def include?(value)
+        @hash.has_key(value)
+    end
+
+    def add(value)
+        self.push(value)
+    end
+
+    def delete(value)
+        self.delete(value)
+    end
+
+    def size
+        self.length
+    end
+
+    
+
+end
